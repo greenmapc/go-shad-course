49272638d2bea699473ed35ca6e3d433f7143449
greenmapc <greenmapc@gmail.com>
2023-08-07 17:24:57 +0200

    solution updated with switch cases

--- retryupdate/update.go
+++ retryupdate/update.go
@@ -15,83 +15,53 @@ func UpdateValue(c kvapi.Client, key string, updateFn func(oldValue *string) (ne
 	response, err := c.Get(&kvapi.GetRequest{
 		Key: key,
 	})
+	var oldValue *string
+	var oldVersion uuid.UUID
+	newVersion := uuid.Must(uuid.NewV4())
 
-	keyNotFound := errors.Is(err, kvapi.ErrKeyNotFound)
+	switch true {
+	case err == nil:
+		oldValue = &response.Value
+		oldVersion = response.Version
+	case errors.Is(err, kvapi.ErrKeyNotFound):
+		oldValue = nil
+	case errors.As(err, &AuthError):
+		return err
+	default:
+		return UpdateValue(c, key, updateFn)
+	}
 
-	if err != nil && errors.As(err, &AuthError) {
+	newValue, err := updateFn(oldValue)
+	if err != nil {
 		return err
 	}
 
-	if keyNotFound || err == nil {
-		var newValue string
+	for {
+		_, setError := c.Set(&kvapi.SetRequest{
+			Key:        key,
+			Value:      newValue,
+			OldVersion: oldVersion,
+			NewVersion: newVersion,
+		})
 
-		if keyNotFound {
-			updateWithNilResult, updateWithNilErr := updateFn(nil)
-			if updateWithNilErr != nil {
-				return updateWithNilErr
-			}
-			newValue = updateWithNilResult
-		} else {
-			updateResult, updateErr := updateFn(&response.Value)
-			if updateErr != nil {
-				return updateErr
-			}
-			newValue = updateResult
-		}
-
-		var oldVersionIfKeyNotFound uuid.UUID
-		var oldVersion uuid.UUID
-		newVersion := uuid.Must(uuid.NewV4())
-
-		if !keyNotFound {
-			oldVersion = response.Version
-		}
-		for {
-			_, setError := c.Set(&kvapi.SetRequest{
-				Key:        key,
-				Value:      newValue,
-				OldVersion: oldVersion,
-				NewVersion: newVersion,
-			})
-
-			if setError != nil && errors.As(setError, &AuthError) {
-				return setError
-			}
-			if errors.Is(setError, kvapi.ErrKeyNotFound) {
-
-				nilValue, err := updateFn(nil)
-				if err != nil {
-					return err
-				}
-				for {
-					_, setRepeatError := c.Set(&kvapi.SetRequest{
-						Key:        key,
-						Value:      nilValue,
-						OldVersion: oldVersionIfKeyNotFound,
-						NewVersion: uuid.Must(uuid.NewV4()),
-					})
-
-					if setRepeatError != nil && errors.As(setRepeatError, &AuthError) {
-						return setRepeatError
-					}
-					if setRepeatError != nil {
-						continue
-					}
-					return nil
-				}
-			}
-			if errors.As(setError, &OutdatedVersion) {
-				if OutdatedVersion.ExpectedVersion == newVersion {
-					return nil
-				}
-				return UpdateValue(c, key, updateFn)
-			}
-			if setError != nil {
-				continue
-			}
+		switch true {
+		case setError == nil:
 			return nil
+		case errors.As(setError, &AuthError):
+			return setError
+		case errors.Is(setError, kvapi.ErrKeyNotFound):
+			newValue, err = updateFn(nil)
+			if err != nil {
+				return err
+			}
+			oldVersion = uuid.UUID{}
+		case errors.As(setError, &OutdatedVersion):
+			if OutdatedVersion.ExpectedVersion == newVersion {
+				return nil
+			}
+			return UpdateValue(c, key, updateFn)
+		default:
+			continue
 		}
-	} else {
-		return UpdateValue(c, key, updateFn)
 	}
 }
